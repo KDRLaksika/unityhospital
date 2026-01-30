d74cb8c6da5d6b2c84f2d82aed59c3ef38b70dff
KDRLaksika <135639511+KDRLaksika@users.noreply.github.com>
2026-01-30 23:52:59 +0530

    Create initial folder structure of unityhospital and manage microservices

---
+++ backend/appointmentservice/src/main/resources/db/migration/V1__baseline.sql
@@ -0,0 +1,1 @@
+-- Baseline migration for appointmentservice (appointmentdb)
---
+++ backend/appointmentservice/src/test/java/com/unityhospital/appointmentservice/AppointmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unityhospital.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
