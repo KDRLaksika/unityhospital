a2474fe3f9b9a4de1a15e994eb642a7cadb9350a
KDG Katuwagala <dinithigayathri1@gmail.com>
2026-02-28 14:47:21 +0530

    Create doctor service

---
+++ backend/doctorservice/src/main/java/com/unityhospital/doctorservice/doctor/dto/DoctorDropdownDto.java
@@ -0,0 +1,9 @@
+package com.unityhospital.doctorservice.doctor.dto;
+
+import java.util.UUID;
+
+public class DoctorDropdownDto {
+    public UUID id;
+    public String name;        // "Dr. X"
+    public String speciality;  // optional but useful
+}
